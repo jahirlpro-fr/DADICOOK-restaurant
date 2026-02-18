10fd196725d5c3ab4b3fc3875d45b42d930eb240
softgenai <admin@softgen.ai>
2026-02-18 11:07:56 +0000

    Edit /next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -53,7 +53,7 @@ const nextConfig = {
                 headers: [
                     {
                         key: 'X-Frame-Options',
-                        value: 'DENY',
+                        value: 'SAMEORIGIN',
                     },
                     {
                         key: 'X-Content-Type-Options',
