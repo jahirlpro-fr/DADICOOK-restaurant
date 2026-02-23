96a96bb753303cef6194340d34fdc64e7cfa096c
softgenai <admin@softgen.ai>
2026-02-23 10:23:44 +0000

    Edit /next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -51,10 +51,7 @@ const nextConfig = {
             {
                 source: '/:path*',
                 headers: [
-                    {
-                        key: 'X-Frame-Options',
-                        value: 'DENY',
-                    },
+
                     {
                         key: 'X-Content-Type-Options',
                         value: 'nosniff',
